36d478b74a5e36ffff12459eb951c64b4abedf8c
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 17:05:00 +0530

    IfElseProblem

---
+++ IfElseProblems/SingleDigitNo.sh
@@ -0,0 +1,35 @@
+read -p "enter single digit number: " x;
+
+if [ $x -eq 0 ]
+then
+        echo "Zero"
+elif [ $x -eq 1 ]
+then
+	echo "One"
+elif [ $x -eq 2 ]
+then
+        echo "Two"
+elif [ $x -eq 3 ]
+then
+        echo "Three"
+elif [ $x -eq 4 ]
+then
+        echo "Four"
+elif [ $x -eq 5 ]
+then
+        echo "Five"
+elif [ $x -eq 6 ]
+then
+        echo "Six"
+elif [ $x -eq 7 ]
+then
+        echo "Seven"
+elif [ $x -eq 8 ]
+then
+        echo "Eight"
+elif [ $x -eq 9 ]
+then
+        echo "Nine"
+else
+	echo "invalid input"
+fi
