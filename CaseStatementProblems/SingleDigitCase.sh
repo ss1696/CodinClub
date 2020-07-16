18be47d76c700b0606f2db6a791bd7277ef03746
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 16:52:18 +0530

    CaseProblems

---
+++ CaseStatementProblems/SingleDigitCase.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "enter single digit number"
+read singleDigit
+case "$singleDigit" in
+	1)	echo "$singleDigit : one";;
+	2)      echo "$singleDigit : two";;
+	3)      echo "$singleDigit : three";;
+	4)      echo "$singleDigit : four";;
+	5)      echo "$singleDigit : five";;
+	6)      echo "$singleDigit : six";;
+	7)      echo "$singleDigit : seven";;
+	8)      echo "$singleDigit : eight";;
+	9)      echo "$singleDigit : nine";;
+esac
